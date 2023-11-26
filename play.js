2dbdae19c93b1bdfc8c711301dff63d296519ee8
Yusuf Mahjoub <y.mahjoub1@outlook.com>
2023-11-26 12:22:10 -0500

    listen for user input

--- play.js
+++ play.js
@@ -6,13 +6,30 @@ const { Game } = require('./src/Game')
 const { UserInterface } = require('./src/UserInterface')
 const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
-
-
 const { connection } = require("./client");
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
 
 
 console.log("connecting...");
 const remoteConnection = connection();
+setupInput();
 
 
 // Begin game
